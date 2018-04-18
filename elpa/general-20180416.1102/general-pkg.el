82a26d68e1e80e4196e91a859effcd3d3233c8e2
Tom Arrell <tom.arrell@gmail.com>
2018-04-18 13:49:02 +1200

    Initial commit

---
+++ elpa/general-20180416.1102/general-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "general" "20180416.1102" "Convenience wrappers for keybindings." '((emacs "24.4") (cl-lib "0.5")) :commit "2145de56f183c79b0a768e068bffcee7ab64b672" :keywords '("vim" "evil" "leader" "keybindings" "keys") :url "https://github.com/noctuid/general.el")
---
+++ init.el
@@ -0,0 +1,89 @@
+;; Personal Emacs Configuration
+
+;;; Author: Tom Arrell <https://github.com/tomarrell>
+;;; Inspired by https://sam217pa.github.io/2016/08/30/how-to-make-your-own-spacemacs/
+
+;; Use-package bootstrapping
+;;; + adding MELPA package archive
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(defconst use-package-verbose t)
+
+(eval-when-compile
+  (require 'use-package))
+
+;; =============================
+;; ------     Plugins     ------
+;; =============================
+
+(use-package evil :ensure t)
+(use-package which-key :ensure t)
+(use-package general :ensure t
+  :config
+  (general-evil-setup t)
+
+  ;; * Prefix Keybindings
+  ;; :prefix can be used to prevent redundant specification of prefix keys
+  ;; again, variables are not necessary and likely not useful if you are only
+  ;; using a definer created with `general-create-definer' for the prefixes
+  ;; (defconst my-leader "SPC")
+  ;; (defconst my-local-leader "SPC m")
+
+  (general-create-definer my-leader-def
+    ;; :prefix my-leader
+    :prefix "SPC")
+
+  (general-create-definer my-local-leader-def
+    ;; :prefix my-local-leader
+    :prefix "SPC m")
+
+  ;; Prevent keymaps from being overridden
+  (general-override-mode)
+
+  ;; Git bindings
+  (my-leader-def 'normal
+    "SPC" '(execute-extended-command :which-key "M-x")
+
+    "g" '(:ignore t :which-key "Git")
+    "gs" '(magit-status :which-key "git status")
+  )
+
+  ;; File bindings
+  (my-leader-def 'normal
+    "f" '(:ignore t :which-key "File")
+    "ff" '(counsel-find-file :which-key "file file")
+  )
+)
+
+;; Allow up and down in results with Vim keybindings
+(use-package ivy :ensure t
+  :config
+  (define-key ivy-minibuffer-map (kbd "C-j") #'ivy-next-line)
+  (define-key ivy-minibuffer-map (kbd "C-k") #'ivy-previous-line)
+  (define-key ivy-minibuffer-map (kbd "C-<return>") #'ivy-immediate-done)
+  (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit))
+
+;; Counsel installs Swiper as dependency
+(use-package counsel :ensure t)
+
+
+;; =============================
+;; ------  Configuration  ------
+;; =============================
+(require 'which-key)
+(which-key-mode)
+(evil-mode 1)
+(counsel-mode 1)
+
+;; Set where the custom variables are stored
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
